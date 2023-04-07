158b69bdd9a3eb9751d3673669750e785667225a
Vlad <vladyslav.balabanov@gmail.com>
2023-04-07 11:32:56 +0200

    Setup spring project.

---
+++ src/main/java/com/bikeshare/back/BikeShareBackTempApplication.java
@@ -0,0 +1,13 @@
+package com.bikeshare.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeShareBackTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeShareBackTempApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bikeshare/back/BikeShareBackTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikeshare.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeShareBackTempApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
